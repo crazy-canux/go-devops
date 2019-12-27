2a1954f30960be80c8b1a6a83ee47343992583bb
Canux CHENG <WCheng@SonicWall.com>
2019-12-27 07:04:50 +0000

    update

--- database/mssql.go
+++ database/mssql.go
@@ -3,14 +3,17 @@ package database
 import (
 	"database/sql"
 	"fmt"
+	"io/ioutil"
 	"log"
+	"strings"
 
 	_ "github.com/denisenkom/go-mssqldb"
 )
 
 func MssqlVersion(host string, port int, database, username, password string) (string, error) {
 	connString := fmt.Sprintf("server=%s;port=%d;database=%s;user id=%s;password=%s;encrypt=disable", host, port, database, username, password)
-	conn, err := sql.Open("mssql", connString)
+	//conn, err := sql.Open("mssql", connString)
+	conn, err := sql.Open("sqlserver", connString)
 	if err != nil {
 		log.Println("Parameter error.")
 		return "", err
@@ -31,4 +34,44 @@ func MssqlVersion(host string, port int, database, username, password string) (s
 	}
 
 	return version, nil
+}
+
+func PatchMssql(host string, port int, user, pw, db string, patch string) bool {
+	connString := fmt.Sprintf("server=%s;port=%d;database=%s;user id=%s;password=%s;encrypt=disable", host, port, db, user, pw)
+	conn, err := sql.Open("mssql", connString)
+	if err != nil {
+		log.Printf("database parameter error: %s", host, err.Error())
+		return false
+	}
+	defer conn.Close()
+
+	err = conn.Ping()
+	if err != nil {
+		log.Printf("Connection error: %s", err.Error())
+		return false
+	}
+
+	fBytes, err := ioutil.ReadFile(patch)
+	if err != nil {
+		log.Printf("open db script error: %s", err.Error())
+		return false
+	}
+
+	sqlSlice1 := strings.Split(string(fBytes), "GO")
+	sqlSlice2 := strings.Split(strings.Join(sqlSlice1, ""), "go")
+	query := strings.Join(sqlSlice2, "")
+
+	stmt, err := conn.Prepare(query)
+	if err != nil {
+		log.Printf("perpare script error: %s", err.Error())
+		return false
+	}
+	defer stmt.Close()
+
+	_, err = stmt.Exec()
+	if err != nil {
+		log.Printf("execute script error: %s", err.Error())
+		return false
+	}
+	return true
 }
--- database/mysql.go
+++ database/mysql.go
@@ -1,9 +1,11 @@
 package database
 
 import (
+	"bytes"
 	"database/sql"
 	"fmt"
 	"log"
+	"os/exec"
 
 	_ "github.com/go-sql-driver/mysql"
 )
@@ -30,4 +32,25 @@ func MysqlVersion(host string, port int, database, username, password string) (s
 		return "", err
 	}
 	return version, nil
+}
+
+func PatchMysql(host string, port int, user, pw, db string, patch string) bool {
+	var stdout, stderr bytes.Buffer
+	h := fmt.Sprintf("-h%s", host)
+	P := fmt.Sprintf("-P%d", port)
+	u := fmt.Sprintf("-u%s", user)
+	p := fmt.Sprintf("-p%s", pw)
+	d := fmt.Sprintf("-D%s", db)
+	query := fmt.Sprintf("source %s", patch)
+	cmd := exec.Command("mysql", h, P, u, p, d, "-e", query)
+	cmd.Stdout = &stdout
+	cmd.Stderr = &stderr
+	err := cmd.Run()
+	log.Printf("stdout: %s", stdout.String())
+	log.Printf("stderr: %s", stderr.String())
+	if err != nil {
+		log.Printf("execute script error: %s", err.Error())
+		return false
+	}
+	return true
 }
